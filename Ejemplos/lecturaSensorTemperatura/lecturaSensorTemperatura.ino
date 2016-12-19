e1dd2b044ddeaa8c40d266c674afc911393c4e4a
javacasm <javacasm@gmail.com>
2016-12-19 16:31:24 +0100

    ejemplo

--- Ejemplos/lecturaSensorTemperatura/lecturaSensorTemperatura.ino
+++ Ejemplos/lecturaSensorTemperatura/lecturaSensorTemperatura.ino
@@ -4,11 +4,25 @@
 */
 void setup() {
 Serial.begin(9600);
+pinMode(7,OUTPUT);
 }
 
 void loop() {
   int valor=analogRead(A0);
-  float temperatura=(valor/1023.0)*500
+  float temperatura=(valor/1023.0)*500;
   Serial.println(temperatura);
+  if(temperatura>28)
+  {
+    digitalWrite(7,HIGH);
+    Serial.println("Activado");
+    delay(10000);
+  }
+
+  if(temperatura<26)
+  {
+    Serial.println("Desactivado");
+    digitalWrite(7,LOW);
+    delay(1000);
+  }
   delay(100);
 }
